bd287344c642aea9d758a8c347c072e0f808c5b7
ExeJeck <andreyluzan1911@gmail.com>
2025-01-16 03:03:40 +0100

    initial commit

--- patient_manage/templatetags/custom_tags.py
+++ patient_manage/templatetags/custom_tags.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def model_name(obj):
     return obj._meta.object_name
+
+
+@register.filter
+def attr(obj, field_name):
+    return getattr(obj, field_name, '')
